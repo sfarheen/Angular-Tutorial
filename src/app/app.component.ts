d6f2a5965a4d551e0a21a4b17e6e07b9a5740d4b
sfarheen <farheensultana2020@gmail.com>
2021-07-02 18:19:47 -0400

    CSS Class Binding in Angular [3 files modified]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  classesToApply: string = 'boldClass italicsClass';
+
+  applyBoldClass: boolean = true;
+  applyItalicsClass: boolean = true;
+
+  addClasses() {
+    let classes = {
+      boldClass: this.applyBoldClass,
+      italicsClass: this.applyItalicsClass
+    };
+    return classes;
+  }
 }
