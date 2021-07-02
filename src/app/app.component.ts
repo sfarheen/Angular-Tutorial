3dc282490c6f779ace674de60484e315dcbd134c
sfarheen <farheensultana2020@gmail.com>
2021-07-02 16:35:53 -0400

    Removing Interpolation and Property Binding Examples [3 files modified]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageHeader: string = 'Employee Details';
-
-  pageHeader2: string = null;
-
-  imagePath: string = '02/pargim-logo-1.png';
-
-  firstName: string = 'Tom';
-  lastName: string = 'Hopkins';
-
-  getFullName(): string {
-    return this.firstName + ' ' + this.lastName;
-  }
-
-  isDisabled: boolean = false;
-
-  badHtml: string = 'Hello <script>alert("Hacked");</script> World';
+  
 }
