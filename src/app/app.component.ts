5939a7bb2ab03ebe6bf7f203519d1c0cfc207bf9
sfarheen <farheensultana2020@gmail.com>
2021-07-02 13:39:06 -0400

    Angular Interpolation Examples [2 files modified]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageHeader: string = 'Employee Details';
+ 
+  pageHeader2: string = null;
+
+  imagePath: string =
+    'https://www.pragimtech.com/wp-content/uploads/2019/02/pargim-logo-1.png';
+
+  firstName: string = 'Tom';
+  lastName: string = 'Hopkins';
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
 }
