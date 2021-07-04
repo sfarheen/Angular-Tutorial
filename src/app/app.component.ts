9a44e4d21a54b570f3f1a83c00459ced3c32cfb3
sfarheen <farheensultana2020@gmail.com>
2021-07-04 18:03:25 -0400

    Event Binding in Angular [4 files modified]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+}) 
 export class AppComponent {
+  onClick(): void {
+    console.log('button clicked');
+  }
 }
