0b16bb8551234860ca83a9cac6068435c87f2daf
sfarheen <farheensultana2020@gmail.com>
2021-07-04 18:16:04 -0400

    Two way Data Binding in Angular [2 files modified]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-}) 
+})
 export class AppComponent {
-  onClick(): void {
-    console.log('button clicked');
-  }
+  name: string = 'Tom';
 }
