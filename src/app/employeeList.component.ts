ac2d2432bfc063be7e15c132b5912c3584f317b2
sfarheen <farheensultana2020@gmail.com>
2021-07-06 16:47:17 -0400

    Removing getEmployees() code [2 files modified]

--- src/app/employeeList.component.ts
+++ src/app/employeeList.component.ts
@@ -37,39 +37,6 @@ export class EmployeeListComponent {
         gender: 'Female',
         annualSalary: 6500.826,
         dateOfBirth: '10/14/1980'
-      }
-    ];
-  }
-
-  getEmployees(): void {
-    this.employees = [
-      {
-        code: 'emp101',
-        name: 'Tom',
-        gender: 'Male',
-        annualSalary: 5500,
-        dateOfBirth: '6/25/1988'
-      },
-      {
-        code: 'emp102',
-        name: 'Alex',
-        gender: 'Male',
-        annualSalary: 5700.95,
-        dateOfBirth: '9/6/1982'
-      },
-      {
-        code: 'emp103',
-        name: 'Mike',
-        gender: 'Male',
-        annualSalary: 5900,
-        dateOfBirth: '12/8/1979'
-      },
-      {
-        code: 'emp104',
-        name: 'Mary',
-        gender: 'Female',
-        annualSalary: 6500.826,
-        dateOfBirth: '10/11/1980'
       },
       {
         code: 'emp105',
@@ -80,10 +47,4 @@ export class EmployeeListComponent {
       }
     ];
   }
-
-  trackByEmpCode(index:number, employee:any): string{
-    return employee.code;
-  }
-
-
 }
